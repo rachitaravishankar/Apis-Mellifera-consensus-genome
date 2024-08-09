a6e51e3b7b6d24ad5772f72921a02cb823345400
Rachita Ravishankar <154069493+rachitaravishankar@users.noreply.github.com>
2024-08-09 12:07:59 +0100

    Update consensus_genome_builder.py

--- consensus_genome_builder.py
+++ consensus_genome_builder.py
@@ -1,16 +1,16 @@
-##Read in the .vcf file using Python pandas package
+# Read in the .vcf file using Python pandas package
 import pandas as pd 
 path=(r'path to .vfc file')
 vcf_full_file=pd.read_csv(path, sep='\t', skiprows=20)
 
-##Query the data contained in the .vcf file with all the Carnica types
+# Query the data contained in the .vcf file with all the Carnica types
 vcf_ff_carnica=vcf_full_file[["#CHROM", "POS", "REF", "ALT","FORMAT","CAR01", "CAR02", "CAR03", "CAR04", "CAR05", "CAR06", "CAR07", "CAR08", "CAR09", "CAR10", "CAR11", "CAR12", "CAR13", "CAR14", "CAR15", "CAR16", "CAR17", "CAR18", "CAR19", "CAR20", "CAR21", "CAR22", "CAR23", "CAR25", "CAR26", "CAR28", "CAR29", "CAR30", "CAR31", "Carnica_BE1", "Carnica_BE10", "Carnica_BE11", "Carnica_BE12", "Carnica_BE2", "Carnica_BE3", "Carnica_BE4", "Carnica_BE5", "Carnica_BE6", "Carnica_BE7", "Carnica_BE8", "Carnica_BE9", "Carnica_SL1", "Carnica_SL10", "Carnica_SL11", "Carnica_SL12", "Carnica_SL13", "Carnica_SL14", "Carnica_SL15", "Carnica_SL16", "Carnica_SL17", "Carnica_SL18", "Carnica_SL2", "Carnica_SL3", "Carnica_SL4", "Carnica_SL5","Carnica_SL6","Carnica_SL7", "Carnica_SL8","Carnica_SL9", "DEU17"]]
 
-##Convert the dataframe into a list
+# Convert the dataframe into a list
 full_list=vcf_ff_carnica.values.tolist()
 
-##Obtain chromsome positions that would need to be replaced
-##50% of the 1/1 occuring through out the Carnica types are taken into account
+# Obtain chromsome positions that would need to be replaced
+    # 50% of the 1/1 occuring through out the Carnica types are taken into account
 carnica_ff_output=open("carnica_changes_fullfile.txt", "w")
 carnica_ff_output.write("#CHROM\tPOS\tREF\tALT\n")
 for line in full_list:
@@ -22,7 +22,7 @@ for line in full_list:
         carnica_ff_output.write(str(w[0])+ "\t" + str(w[1])+ "\t"+ str(w[2])+ "\t" + str(w[3]) + "\n")
 carnica_ff_output.close()
 
-##Create a Python dictionary containing information of the alleles that would need to be replaced
+# Create a Python dictionary containing information of the alleles that would need to be replaced
 chromosome_dict={}
 with open("carnica_changes_fullfile.txt","r") as file:
     for line in file:
@@ -34,8 +34,8 @@ with open("carnica_changes_fullfile.txt","r") as file:
             else:
                 i_dict.update({line[1]: (line[2], line[3])})
 
-##Use BioPython package SeqIO to contrsuct the consensus genome
-##A loop is created through the python dictionary to combine the A.mellifera reference genome and the new sequence with replaced alleles
+# Use BioPython package SeqIO to construct the consensus genome
+# A loop is created through the python dictionary to combine the A.mellifera reference genome and the new sequence containing the replaced alleles
 from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
